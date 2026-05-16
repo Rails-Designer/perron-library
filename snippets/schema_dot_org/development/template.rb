5642292cf389865a8d033a51f09493d4b0bb3faa
e <988051+eelcoj@users.noreply.github.com>
2026-05-16 10:38:46 +0000

    Snippet for schema.org (#1)
    * Adds snippet to add schema.org structured data (based on example from
    @ianyamey)
    
    * Update

--- snippets/schema_dot_org/development/template.rb
+++ snippets/schema_dot_org/development/template.rb
@@ -1,13 +1,11 @@
 gem "perron" unless File.read("Gemfile").include?("perron")
 
+gem "schema_dot_org", "~> 2.5" unless File.read("Gemfile").include?("schema_dot_org")
+
 after_bundle do
   unless File.exist?("config/initializers/perron.rb")
     rails_command "perron:install"
   end
 
-key = SecureRandom.hex
-
-file "public/#{key}.txt", <<-KEY
-#{key}
-KEY
+  {{files}}
 end
